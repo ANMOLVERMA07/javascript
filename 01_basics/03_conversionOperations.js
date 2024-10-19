657a3517f1c6233a9efa837b6c5eda8b4d07834e
ANMOLVERMA07 <153612130+ANMOLVERMA07@users.noreply.github.com>
2024-10-19 07:09:29 +0000

    operations knowledge

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,68 @@
+let score  = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "anmol" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************************** OPERATIONS ****************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "harshit"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+/*
+ if string comes first, then it convert it all inot string
+ if number comes first , then it do airthmetic operations first and then convert it into string
+*/
+
+// console.log(+true); // => 1
+
+// console.log(+""); // => 0
+
+let num1, num2 , num3 
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
