23a25eaf55941bfc72780bdcff6ec11a8e30b45c
ANMOLVERMA07 <153612130+ANMOLVERMA07@users.noreply.github.com>
2024-10-19 04:19:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS Code as newer version
+
+let name = "Anmol" // String
+let age = 17 // Number
+let isLoggedIn = true // boolean
+
+let state; // undefined
+
+let pass = null; // null - standalone value
+
+// symbol => unique
+
+console.log(typeof "anmol")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
