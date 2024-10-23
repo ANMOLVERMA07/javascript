36f182a4f19c3cfd4ffcd4f720849bfbcb67bcd6
ANMOLVERMA07 <153612130+ANMOLVERMA07@users.noreply.github.com>
2024-10-23 16:37:29 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+   console.log("A");
+   console.log("N");
+   console.log("M");
+   console.log("O");
+   console.log("L");
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1,number2){
+    console.log(number1 + number2);
+    
+}
+
+addTwoNumbers(3,4);        // 7
